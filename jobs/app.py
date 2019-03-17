5f570041731686a9a5cbcca182144205b96b355b
whitneychild <42783775+whitneychild@users.noreply.github.com>
2019-03-16 21:55:37 -0600

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,7 @@ def open_connection():
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
-    return _connection
+    return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
         connection = open_connection()
